dd6afdd54b56152acbc0e357142fa14c8fe095d5
Vsevolod Zhmurov <sevajmurov@gmail.com>
2019-09-08 23:58:50 +0300

    Pie chart added.

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,15 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  public pieChartData: number[] = [350, 450, 120];
+  public pieChartLabels: string[] = ['Pretzel industries', 'Doughnut logistics', 'Cucumber aerospace'];
+  public pieChartColors: any[] = [
+      {
+        backgroundColor: ['#26547c', '#ff6b6b', '#ffd166']
+      }
+  ];
+  public pieChartType = 'pie';
+
   ngOnInit() {
   }
 
