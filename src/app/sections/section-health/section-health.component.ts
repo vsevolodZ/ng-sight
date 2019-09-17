ac09c80a16dd072dd7b5777e466877b62372bca9
Vsevolod <vsevolod@oneplus_6.mont.ru>
2019-09-17 17:56:10 +0300

    Section system health.

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Server } from '../../shared/server';
+
+const SAMPLE_SERVERS = [
+{id: 1, name: "cosmos.mont.ru", isOnline: true},
+{id: 2, name: "phobos.mont.ru", isOnline: true},
+{id: 3, name: "hubble.mont.ru", isOnline: false},
+{id: 4, name: "lotus.mont.ru", isOnline: true},
+{id: 5, name: "deimos.mont.ru", isOnline: true},
+{id: 6, name: "disciple.mont.ru", isOnline: false},
+{id: 7, name: "ansible.mont.ru", isOnline: true},
+{id: 8, name: "apache.mont.ru", isOnline: true},
+];
 
 @Component({
   selector: 'app-section-health',
@@ -8,6 +20,9 @@ import { Component, OnInit } from '@angular/core';
 export class SectionHealthComponent implements OnInit {
 
   constructor() { }
+
+  servers: Server[] = SAMPLE_SERVERS;
+
 
   ngOnInit() {
   }
